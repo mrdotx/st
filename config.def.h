50ec6e5f6fc281eff759e5c33a3ecddc93890d82
mrdotx <klassiker@gmx.de>
2019-06-11 17:30:41 +0200

    update config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=16:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*

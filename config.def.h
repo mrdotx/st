b6500fd462cf4891428757e4f199b96dce1c0b5a
mrdotx <klassiker@gmx.de>
2019-06-12 20:42:07 +0200

    patch keyboard select

--- config.def.h
+++ config.def.h
@@ -239,6 +239,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+    { ControlMask,          XK_Escape,      keyboard_select,{ 0 } },
 	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },

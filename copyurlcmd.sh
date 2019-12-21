95dbc46fabb994542d8dddbbff03fbb88af8b174
mrdotx <klassiker@gmx.de>
2019-12-21 16:17:41 +0100

    move to projects

--- copyurlcmd.sh
+++ copyurlcmd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# path:       ~/coding/st/copyurlcmd.sh
+# path:       ~/projects/st/copyurlcmd.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/st
-# date:       2019-12-21 13:22:50
+# date:       2019-12-21 15:04:05
 
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
 url=$(grep -Po "$regex" | rofi -monitor -2 -theme klassiker-vertical -dmenu -i -p "Copy which url?:" -w "$WINDOWID" -l 5) || exit
-echo "$url" | xsel -b && notify-send -i "$HOME/coding/st/icons/terminal.png" "$TERMINAL" "URL: $url\ncopied to clipboard"
+echo "$url" | xsel -b && notify-send -i "$HOME/projects/st/icons/terminal.png" "$TERMINAL" "URL: $url\ncopied to clipboard"
--- openurlcmd.sh
+++ openurlcmd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# path:       ~/coding/st/openurlcmd.sh
+# path:       ~/projects/st/openurlcmd.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/st
-# date:       2019-12-01 12:07:15
+# date:       2019-12-21 15:04:17
 
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
 url=$(grep -Po "$regex" | rofi -monitor -2 -theme klassiker-vertical -dmenu -i -p "Follow which url?:" -w "$WINDOWID" -l 5) || exit
-$BROWSER "$url" & notify-send -i "$HOME/coding/st/icons/terminal.png" "$TERMINAL" "URL: $url\nopen in $BROWSER"
+$BROWSER "$url" & notify-send -i "$HOME/projects/st/icons/terminal.png" "$TERMINAL" "URL: $url\nopen in $BROWSER"

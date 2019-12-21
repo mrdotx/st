ce39debaeac178431786b4e4ec07b9c59a3f76c4
mrdotx <klassiker@gmx.de>
2019-12-21 14:08:37 +0100

    remove xclip

--- copycmdout.sh
+++ copycmdout.sh
@@ -3,7 +3,7 @@
 # path:       ~/coding/st/copycmdout.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/st
-# date:       2019-12-01 12:07:29
+# date:       2019-12-21 13:22:39
 
 tmpfile=$(mktemp /tmp/st-cmd-output.XXXXXX)
 trap 'rm "$tmpfile"' 0 1 15
@@ -11,4 +11,4 @@ sed -n "w $tmpfile"
 ps1="$(grep "\S" "$tmpfile" | tail -n 1 | sed 's/^\s*//' | cut -d' ' -f1)"
 chosen="$(grep -F "$ps1" "$tmpfile" | sed '$ d' | tac | rofi -monitor -2 -theme klassiker-vertical -dmenu -i -p "Copy which command's output?" -l 5 | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
 eps1="$(echo "$ps1" | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
-awk "/^$chosen$/{p=1;print;next} p&&/$eps1/{p=0};p" "$tmpfile" | xclip -selection clipboard && notify-send -i "$HOME/coding/st/icons/terminal.png" "$TERMINAL" "Command output copied to clipboard"
+awk "/^$chosen$/{p=1;print;next} p&&/$eps1/{p=0};p" "$tmpfile" | xsel -b && notify-send -i "$HOME/coding/st/icons/terminal.png" "$TERMINAL" "Command output copied to clipboard"

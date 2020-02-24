61ffc649241f4c965cb711c63fff58d22a2f32a7
mrdotx <klassiker@gmx.de>
2020-02-24 12:22:36 +0100

    small optimizations

--- copycmdout.sh
+++ copycmdout.sh
@@ -3,12 +3,13 @@
 # path:       ~/projects/st/copycmdout.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/st
-# date:       2020-02-03T13:48:05+0100
+# date:       2020-02-24T12:18:36+0100
 
 tmpfile=$(mktemp /tmp/st-cmd-output.XXXXXX)
 trap 'rm "$tmpfile"' 0 1 15
 sed -n "w $tmpfile"
 ps1="$(grep "\S" "$tmpfile" | tail -n 1 | sed 's/^\s*//' | cut -d' ' -f1)"
 chosen="$(grep -F "$ps1" "$tmpfile" | sed '$ d' | tac | rofi -monitor -2 -theme klassiker-vertical -dmenu -i -p "Copy which command's output?" -l 5 | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
-eps1="$(echo "$ps1" | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
-awk "/^$chosen$/{p=1;print;next} p&&/$eps1/{p=0};p" "$tmpfile" | xsel -b && notify-send "$TERMINAL" "Command output copied to clipboard"
+eps1="$(printf "%s\n" "$ps1" | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
+output="$(awk "/^$chosen$/{p=1;print;next} p&&/$eps1/{p=0};p" "$tmpfile")"
+xsel -b "$output" && notify-send "$TERMINAL" "Command copied to clipboard:\n$output"

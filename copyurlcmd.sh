2907d407c78f62e94dd56ff99fc03f86b8b6d068
mrdotx <klassiker@gmx.de>
2020-02-26 17:18:04 +0100

    small fixes

--- copyurlcmd.sh
+++ copyurlcmd.sh
@@ -3,8 +3,8 @@
 # path:       ~/projects/st/copyurlcmd.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/st
-# date:       2020-02-24T12:20:59+0100
+# date:       2020-02-26T17:15:01+0100
 
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
 url=$(grep -Po "$regex" | rofi -monitor -2 -theme klassiker-vertical -dmenu -i -p "Copy which url?:" -w "$WINDOWID" -l 5) || exit
-xsel -b "$url" && notify-send "$TERMINAL" "URL: $url\ncopied to clipboard"
+printf "$url" | xsel -b && notify-send "$TERMINAL" "URL: $url\ncopied to clipboard"

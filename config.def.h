68175be7ea07e2027629cc738950843849314911
mrdotx <klassiker@gmx.de>
2020-04-27 15:34:48 +0200

    update to version 0.8.3

--- config.def.h
+++ config.def.h
@@ -213,8 +213,8 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"}  },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"}  },
+	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
+	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */

82bc5f953ce699067ee42d257e298a02952f6116
mrdotx <klassiker@gmx.de>
2020-05-22 11:04:34 +0200

    patch xresources added

--- config.def.h
+++ config.def.h
@@ -5,11 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
-static char *font2[] = {
-	"JoyPixels:pixelsize=14:antialias=true:autohint=true",
-};
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 
 static int borderpx = 2;
 
@@ -151,6 +149,45 @@ static unsigned int defaultrcs = 256;
  * 7: Snowman ("☃")
  */
 static unsigned int cursorshape = 2;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "font",         STRING,  &font },
+		{ "fontalt",      STRING,  &font2[0] },
+		{ "color0",       STRING,  &colorname[0] },
+		{ "color1",       STRING,  &colorname[1] },
+		{ "color2",       STRING,  &colorname[2] },
+		{ "color3",       STRING,  &colorname[3] },
+		{ "color4",       STRING,  &colorname[4] },
+		{ "color5",       STRING,  &colorname[5] },
+		{ "color6",       STRING,  &colorname[6] },
+		{ "color7",       STRING,  &colorname[7] },
+		{ "color8",       STRING,  &colorname[8] },
+		{ "color9",       STRING,  &colorname[9] },
+		{ "color10",      STRING,  &colorname[10] },
+		{ "color11",      STRING,  &colorname[11] },
+		{ "color12",      STRING,  &colorname[12] },
+		{ "color13",      STRING,  &colorname[13] },
+		{ "color14",      STRING,  &colorname[14] },
+		{ "color15",      STRING,  &colorname[15] },
+		{ "background",   STRING,  &colorname[256] },
+		{ "foreground",   STRING,  &colorname[257] },
+		{ "cursorColor",  STRING,  &colorname[258] },
+		{ "termname",     STRING,  &termname },
+		{ "shell",        STRING,  &shell },
+		{ "scroll",       STRING,  &scroll },
+		{ "blinktimeout", INTEGER, &blinktimeout },
+		{ "bellvolume",   INTEGER, &bellvolume },
+		{ "tabspaces",    INTEGER, &tabspaces },
+		{ "borderpx",     INTEGER, &borderpx },
+		{ "minlatency",   FLOAT,   &minlatency },
+		{ "maxlatency",   FLOAT,   &maxlatency },
+		{ "cwscale",      FLOAT,   &cwscale },
+		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",        FLOAT,   &alpha },
+};
 
 /*
  * Default columns and rows numbers

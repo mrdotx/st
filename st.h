afd107a340f5ff8c5b8a50f7ff1d1c2a0936ab7d
mrdotx <klassiker@gmx.de>
2020-06-02 10:42:53 +0200

    temporary go back to scrollback patches

--- st.h
+++ st.h
@@ -82,6 +82,8 @@ void redraw(void);
 void draw(void);
 
 void opencopied(const Arg *);
+void kscrolldown(const Arg *);
+void kscrollup(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
@@ -89,6 +91,7 @@ void toggleprinter(const Arg *);
 void copyurl(const Arg *);
 
 int tattrset(int);
+int tisaltscr(void);
 void tnew(int, int);
 void tresize(int, int);
 void tsetdirtattr(int);

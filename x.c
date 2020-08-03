f20c93aad5d29c368d94f4e6112396bd5c9acb7d
mrdotx <klassiker@gmx.de>
2020-08-03 09:39:28 +0200

    patch swapmouse added

--- x.c
+++ x.c
@@ -276,6 +276,9 @@ static char *opt_title = NULL;
 static int invertcolors = 0;
 static int oldbutton = 3; /* button event on startup: 3 = release */
 
+static Cursor cursor;
+static XColor xmousefg, xmousebg;
+
 void
 invert(const Arg *dummy)
 {
@@ -1250,10 +1253,8 @@ void
 xinit(int cols, int rows)
 {
 	XGCValues gcvalues;
-	Cursor cursor;
 	Window parent;
 	pid_t thispid = getpid();
-	XColor xmousefg, xmousebg;
 	XWindowAttributes attr;
 	XVisualInfo vis;
 
@@ -1850,6 +1851,12 @@ xsetmode(int set, unsigned int flags)
 {
 	int mode = win.mode;
 	MODBIT(win.mode, set, flags);
+        if (flags & MODE_MOUSE) {
+                if (win.mode & MODE_MOUSE)
+                        XUndefineCursor(xw.dpy, xw.win);
+                else
+                        XDefineCursor(xw.dpy, xw.win, cursor);
+        }
 	if ((win.mode & MODE_REVERSE) != (mode & MODE_REVERSE))
 		redraw();
 }

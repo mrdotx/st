7d17726248f3815745f08653e14545cf206ca046
mrdotx <klassiker@gmx.de>
2020-08-20 18:27:04 +0200

    mouse scroll speed increased

--- config.def.h
+++ config.def.h
@@ -229,8 +229,8 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 5},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 5},      0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ XK_NO_MOD,            Button4, ttysend,        {.s = "\031"} },
 	{ XK_NO_MOD,            Button5, ttysend,        {.s = "\005"} },

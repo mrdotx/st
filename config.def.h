f39f55a92ca15a39af6900b1bef282d71d1ea009
mrdotx <klassiker@gmx.de>
2021-05-12 13:42:56 +0200

    added keyboard select patch

--- config.def.h
+++ config.def.h
@@ -256,6 +256,7 @@ static Shortcut shortcuts[] = {
     { TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
     { ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
     { TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_Escape,      keyboard_select,{.i =  0} },
     { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
     { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
     { MODKEY,               XK_y,           copyurl,        {.i =  0} },
